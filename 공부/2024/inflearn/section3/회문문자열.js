290e97a45f2ef9e175536614e0b0133e0ac4fcae
Dodo <dodo@yanadoocorp.com>
2024-05-20 16:39:35 +0900

    [인프런] section3 - 회문문자열

--- 공부/2024/inflearn/section3/회문문자열.js
+++ 공부/2024/inflearn/section3/회문문자열.js
@@ -1,4 +1,16 @@
-function solution(s) {}
+function solution(s) {
+  const arr = s.toLowerCase().split("");
+  let flag = 1;
+
+  for (let i = 0; i < s.length; i++) {
+    if (arr[i] !== arr[arr.length - i - 1]) {
+      flag = 0;
+      break;
+    }
+  }
+
+  return flag ? "YES" : "NO";
+}
 
 let str = "goooG";
 console.log(solution(str));
