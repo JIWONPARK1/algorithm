fb4777477936904ce38f08c28309be2104a6ae16
Dodo <dodo@yanadoocorp.com>
2024-05-21 16:49:31 +0900

    [인프런] section3 진행중

--- 공부/2024/inflearn/section3/가장짧은문자거리.js
+++ 공부/2024/inflearn/section3/가장짧은문자거리.js
@@ -1,4 +1,17 @@
-function solution(s, t) {}
+/**
+ * 한 개의 문자열 s와 문자 t가 주어지면 문자열 s의 각 문자가 문자 t와 떨어진 최소거리를 출력하는 프로그램을 작성하세요.
+ */
+
+function solution(s, t) {
+  const arr = s.split(t);
+  let answer = "";
+  for (let e of arr) {
+    for (let i = 0; i < e.length; i++) {
+      console.log(i, e[i]);
+    }
+  }
+  return answer;
+}
 
 let str = "teachermode";
 console.log(solution(str, "e"));
