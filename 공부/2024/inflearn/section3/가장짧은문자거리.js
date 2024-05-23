1701d1a105cc3c6960c5f1cae36e7cc1077306e7
Dodo <dodo@yanadoocorp.com>
2024-05-23 14:01:39 +0900

    [인프런] section3 완료

--- 공부/2024/inflearn/section3/가장짧은문자거리.js
+++ 공부/2024/inflearn/section3/가장짧은문자거리.js
@@ -4,14 +4,70 @@
 
 function solution(s, t) {
   const arr = s.split(t);
+  arr.pop();
   let answer = "";
+
   for (let e of arr) {
-    for (let i = 0; i < e.length; i++) {
-      console.log(i, e[i]);
+    const n = e.length;
+    for (let i = 0; i < n; i++) {
+      let index = e.indexOf(e[i]);
+      answer += Math.min(index, n - 1 - index) + 1;
+    }
+    answer += 0;
+  }
+  return answer;
+}
+
+function solution2(s, t) {
+  const n = s.length;
+  let answer = "";
+  let answer1 = "";
+
+  for (let i = 0; i < n; i++) {
+    if (s[i] === t) answer1 += 0;
+    else if (answer1[i - 1]) {
+      answer1 += Number(answer1[i - 1]) + 1;
+    } else {
+      answer1 += 1;
+    }
+  }
+
+  for (let i = n - 1; i >= 0; i--) {
+    if (Number(answer[0]) < answer1[i]) {
+      answer = Number(answer[0]) + 1 + answer;
+    } else {
+      answer = answer1[i] + answer;
     }
   }
   return answer;
 }
 
+function masterSolution(s, t) {
+  let answer = [];
+  let p = 10000;
+  for (let str of s) {
+    if (str === t) {
+      p = 0;
+      answer.push(0);
+    } else {
+      p++;
+      answer.push(p);
+    }
+  }
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] === t) {
+      p = 0;
+    } else {
+      p++;
+      if (answer[i] > p) {
+        answer[i] = p;
+      }
+    }
+  }
+
+  console.log(answer.join(""));
+}
+
 let str = "teachermode";
-console.log(solution(str, "e"));
+
+console.log(masterSolution(str, "e"));
--- 공부/2024/inflearn/section3/문자열압축.js
+++ 공부/2024/inflearn/section3/문자열압축.js
@@ -1,4 +1,25 @@
-function solution(s) {}
+/**
+ * 알파벳 대문자로 이루어진 문자열을 입력받아 같은 문자가 연속으로 반복되는 경우 반복되는 문자 바로 오른쪽에 반복 횟수를 표기하는 방법으로 문자열을 압축하는 프로그램을 작성하시 오.
+ *  단 반복횟수가 1인 경우 생략합니다.
+ */
+
+function solution(s) {
+  let answer = "";
+  let p = 1;
+
+  for (let i = 0; i < s.length; i++) {
+    //이전 글자와 같은 경우 p만 증가
+    if (s[i] === s[i - 1]) {
+      p++;
+    } else {
+      // 같지 않은 경우 글자 추가 & p 0으로 초기화
+      if (p > 1) answer += p;
+      answer += s[i];
+      p = 1;
+    }
+  }
+  return answer;
+}
 
 let str = "KKHSSSSSSSE";
 console.log(solution(str));
