31f0e5052693933946f12b63270ef87f43438a7d
Dodo <dodo@yanadoocorp.com>
2024-10-10 17:52:13 +0900

    [인프런] 241010  완료

--- 공부/2024/inflearn/section4/멘토링.js
+++ 공부/2024/inflearn/section4/멘토링.js
@@ -1,8 +1,38 @@
-function solution(test) {}
+/**
+ * 현수네 반 선생님은 반 학생들의 수학점수를 향상시키기 위해 멘토링 시스템을 만들려고 합니다.
+ * 멘토링은 멘토(도와주는 학생)와 멘티(도움을 받는 학생)가 한 짝이 되어 멘토가 멘티의 수학공부를 도와주는 것입니다.
+ * 선생님은 M번의 수학테스트 등수를 가지고 멘토와 멘티를 정합니다.
+ * 만약 A학생이 멘토이고, B학생이 멘티가 되는 짝이 되었다면, A학생은 M번의 수학테스트에서 모두 B학생보다 등수가 앞서야 합니다.
+ * M번의 수학성적이 주어지면 멘토와 멘티가 되는 짝을 만들 수 있는 경우가 총 몇 가지 인지 출력하는 프로그램을 작성하세요.
+ *
+ * 첫 번째 줄에 반 학생 수 N(1<=N<=20)과 M(1<=M<=10)이 주어진다.
+ * 두 번째 줄부터 M개의 줄에 걸쳐 수학테스트 결과가 학생번호로 주어진다. 학생번호가 제일 앞에서부터 1등, 2등, ...N등 순으로 표현된다.
+ * 만약 한 줄에 N=4이고, 테스트 결과가 3 1 4 2로 입력되었다면 3번 학생이 1등, 1번 학생이 2등, 4번 학생이 3등, 2번 학생이 4등을 의미합니다.
+ */
+function solution(test) {
+  const m = test.length;
+  const n = test[0].length;
+  const map = new Map();
+  let result = 0;
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      let grade = test[i][j];
+      if (map.get(grade)) {
+        const list = map.get(grade);
+        const newList = list.filter((e) => test[i].slice(j + 1).includes(e));
+        map.set(grade, newList);
+      } else {
+        map.set(grade, test[i].slice(j + 1) || []);
+      }
+    }
+  }
+  map.forEach((e) => (result += e.length));
+  return result;
+}
 
 let arr = [
-  [3, 4, 1, 2],
-  [4, 3, 2, 1],
-  [3, 1, 4, 2],
+  [1, 2, 3, 4, 5],
+  [5, 4, 3, 2, 1],
 ];
 console.log(solution(arr));
--- 공부/2024/inflearn/section4/졸업선물.js
+++ 공부/2024/inflearn/section4/졸업선물.js
@@ -1,10 +1,82 @@
-function solution(m, product) {}
+/**
+ * 선생님은 올해 졸업하는 반 학생들에게 졸업선물을 주려고 합니다.
+ * 학생들에게 인터넷 쇼핑몰에서 각자 원하는 상품을 골라 그 상품의 가격과 배송비를 제출하라고 했습니다.
+ * 선생님이 가지고 있는 예산은 한정되어 있습니다.
+ * 현재 예산으로 최대 몇 명의 학생에게 선물을 사줄 수 있는지 구하는 프로그램을 작성하세요.
+ * 선생님은 상품 하나를 50% 할인해서(반 가격) 살 수 있는 쿠폰을 가지고 있습니다. 배송비는 할인에 포함되지 않습니다.
+ *
+ * ▣ 입력설명
+ * 첫 번째 줄에 반 학생수 N(1<=N<=1000)과 예산 M(1<=M<=100,000,000)이 주어진다.
+ * 두 번째 줄부터 N줄에 걸쳐 각 학생들이 받고 싶은 상품의 가격과 배송비가 입력됩니다. 상품가격과 배송비는 각각 100,000을 넘지 않습니다.
+ * 상품가격은 짝수로만 입력됩니다.
+ *
+ * ▣ 출력설명
+ * 첫 번째 줄에 선생님이 현재 예산으로 선물할 수 있는 최대 학생수를 출력합니다. 선생님 최소한 1개 이상의 상품을 살 수 있는 예산을 가지고 있습니다.
+ */
 
-let arr = [
-  [6, 6],
-  [2, 2],
-  [4, 3],
-  [4, 5],
-  [10, 3],
-];
-console.log(solution(28, arr));
+function solution(m, product) {
+  let parseList = [];
+  let i = 0;
+  let limit = m;
+  let result1 = 0;
+  let result2 = 1;
+
+  parseList = product.map((e, i) => ({
+    total: e[0] + e[1],
+    productPrice: e[0],
+    deliveryPrice: e[1],
+    discountPrice: e[0] / 2 + e[1],
+    gap: e[0] + e[1] - (e[0] / 2 + e[1]),
+  }));
+
+  // case1 :  총합이 낮은 순서대로 조합하기
+  const sortedTotals = parseList.sort((a, b) => a.total - b.total);
+
+  while (limit >= sortedTotals[i].total) {
+    result1++;
+    limit -= sortedTotals[i].total;
+    i++;
+  }
+
+  const lastItem = sortedTotals[i];
+  if (lastItem) {
+    const lastItemTotal = lastItem.productPrice / 2 + lastItem.deliveryPrice;
+    if (limit >= lastItemTotal) {
+      result1++;
+    }
+  }
+
+  // case2 :  할인율이 높은 것을 먼저 제하고 총합이 낮은 순서대로 제하기
+  let sortedDiscounts = parseList.sort((a, b) => b.gap - a.gap);
+  limit = m - sortedDiscounts[0].discountPrice;
+  sortedDiscounts.shift();
+  i = 0;
+  sortedDiscounts = sortedDiscounts.sort((a, b) => a.total - b.total);
+
+  while (limit >= sortedDiscounts[i]?.total) {
+    result2++;
+    limit -= sortedDiscounts[i].total;
+    i++;
+  }
+  return result1 > result2 ? result1 : result2;
+}
+
+console.log(
+  solution(596, [
+    [57, 3],
+    [85, 3],
+    [61, 3],
+    [98, 3],
+    [85, 4],
+    [47, 3],
+    [42, 4],
+    [95, 3],
+    [85, 3],
+    [82, 3],
+    [41, 4],
+    [64, 4],
+    [36, 4],
+    [100, 4],
+    [66, 3],
+  ])
+);
